c12b4ad6bc883cc2ed0bec9aed3ef4889c514aea
Lukasz Korzen <korzenlukasz@gmail.com>
2013-07-08 22:10:45 +0200

    Added index.html handling to web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var file = fs.readFileSync('index.html');
+var buffer = new Buffer(file);
+
+var html = buffer.toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
